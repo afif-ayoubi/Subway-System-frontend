88828e381209176c6de46fa4e87bf9165bbfda0a
afif-ayoubi <afifayoubi22@gmail.com>
2024-03-30 03:19:06 +0200

    initialize react app

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./styles/colors.css";
+import "./styles/utilities.css";
+
+import Authentication from "./MainWebsite/Pages/Authentication";
+import Booking from "./MainWebsite/Pages/Booking";
+import Landing from "./MainWebsite/Pages/Landing";
+import Map from "./MainWebsite/Pages/Map";
+import Profile from "./MainWebsite/Pages/Profile";
+import Schedule from "./MainWebsite/Pages/Schedule";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/Authentication" element={<Authentication />} />
+          <Route path="/Booking" element={<Booking />} />
+          <Route path="/Map" element={<Map />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/Schedule" element={<Schedule />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MainWebsite/Pages/Authentication/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Authentication = () => {
+  return (
+    <div>
+      <h1>Authentication</h1>
+    </div>
+  );
+};
+export default Authentication;
