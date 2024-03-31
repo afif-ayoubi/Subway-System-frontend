8d25e544e69d077e0c802e33fd48c4180079b74e
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-03-31 21:00:34 +0300

    update side bar

--- src/MainWebsite/Pages/AdminPanel/component/sidebar/sidebar.jsx
+++ src/MainWebsite/Pages/AdminPanel/component/sidebar/sidebar.jsx
@@ -1,19 +1,32 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faBars , faBuilding,faCoins, faUser} from '@fortawesome/free-solid-svg-icons'
 
 const SideBar =()=>{
     return(
         <div className="side-bar">
-            <div className="img">
-                <img src="./admin.jpg" alt="" />
+            <div className="img flex center">
+                <h1>Admin</h1>
             </div>
-            <div className="links flex center ">
-                <ul>
-                    <li><h2>Dashboard</h2></li>
-                    <li><h2>Branches</h2> </li>
-                    <li><h2>Manager</h2> </li>
-                    <li><h2>Coins</h2> </li>
-                    <li><h2>Log Out</h2> </li> 
-                </ul>
+            <div className="links flex column center ">
+            <ul>
+                <li className="items flex ">
+                    <FontAwesomeIcon icon={faBars} className="icon1" style={{color: "#ffffff", fontSize : "25px"}} />
+                    <h2>Dashboard</h2>
+                </li>
+                <li className="items flex">
+                    <FontAwesomeIcon icon={faBuilding}  className="icon2" style={{color: "#ffffff", fontSize : "25px"}} />
+                    <h2>Branches</h2>
+                </li>
+                <li className="items flex">
+                    <FontAwesomeIcon icon={faCoins}  className="icon3" style={{color: "#ffffff", fontSize : "25px"}} />
+                    <h2>Coins</h2>
+                </li>
+                <li className="items flex">
+                    <FontAwesomeIcon icon={faUser}  className="icon4" style={{color: "#ffffff", fontSize : "25px"}} />
+                    <h2>Manager</h2>
+                </li>
+            </ul>
             </div>
         </div>
     )
