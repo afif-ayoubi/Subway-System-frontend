b79750c17f760b7c5eb60d8c02a58a68182d2b5d
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-03-31 21:00:20 +0300

    update dashboard

--- src/MainWebsite/Pages/AdminPanel/component/Dashborad/dashboard.jsx
+++ src/MainWebsite/Pages/AdminPanel/component/Dashborad/dashboard.jsx
@@ -9,21 +9,51 @@ const Dashboard = ()=>{
     return (
         <div className="main-dashborad">
             <div className="search flex ">
-                {/* <div className="child">
-                <FontAwesomeIcon icon={faBars}  style={{color: "#ffffff",fontSize : "25px"}} />
-                </div> */}
                 <div className="child">
-                    <input type="text" />
+                <FontAwesomeIcon icon={faBars}  style={{color: "black",fontSize : "25px"}} />
                 </div>
                 <div className="child">
-                    <img src="" alt="" />
+                    <input type="text" placeholder="Search" />
+                </div>
+                <div className=" ">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrq8a6plyTZvi2bjCyHBnNKwahQzDOCze830QtQGD6jw&s" alt="" />
                 </div>
                 
             </div>
             <div className="cards flex">
-                <div className="card"></div>
-                <div className="card"></div>
-                <div className="card"></div>
+                <div className="card flex column">
+                    <div>
+                        <h3>Total Rides</h3>
+                    </div>
+                    <div className="child-card flex">
+                        <h3>3568</h3>
+                    </div>
+                </div>
+                <div className="card flex column">
+                    <div>
+                        <h3>Total Rides</h3>
+                    </div>
+                    <div className="child-card flex">
+                        <h3>3568</h3>
+                    </div>
+                </div>
+                <div className="card flex column">
+                    <div>
+                        <h3>Total Rides</h3>
+                    </div>
+                    <div className="child-card flex">
+                        <h3>3568</h3>
+                    </div>
+                </div>
+                <div className="card flex column">
+                    <div>
+                        <h3>Total Rides</h3>
+                    </div>
+                    <div className="child-card flex">
+                        <h3>3568</h3>
+                    </div>
+                </div>
+                
             </div>
             <div className="charts flex">
                 <div className="features">
