8f2ab45e66f768c07b6374ce36aece72c27805a9
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-01 02:58:56 +0300

    update index

--- src/MainWebsite/Pages/AdminPanel/index.jsx
+++ src/MainWebsite/Pages/AdminPanel/index.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import SideBar from "./component/sidebar/sidebar";
 import Dashboard from "./component/Dashborad/dashboard";
+import Branches from "./component/Branches/Branch";
 import "./style.css"
+import "./component/charts/feature.css"
+import "./component/charts/chart.css"
+import "./component/Branches/branch.css"
+import { BrowserRouter,Routesm,Route } from "react-router-dom";
 
 const AdminPanel = ()=>{
 
     return (
         <div className="container flex ">
-            
             <SideBar />
-            <Dashboard />
+            {/* <Dashboard /> */}
+            <Branches />
         </div>
     )
 }
