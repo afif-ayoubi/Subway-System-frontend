e5b46e604040d68ac84fba67c9a6de6ff5d3f435
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-03-31 21:00:05 +0300

    add feature jsx

--- src/MainWebsite/Pages/AdminPanel/component/charts/feature.jsx
+++ src/MainWebsite/Pages/AdminPanel/component/charts/feature.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faBars, faEllipsisVertical} from '@fortawesome/free-solid-svg-icons'
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 const Features = ()=>{
 
     return (
-        <div>Features</div>
+        <div className="Featured ">
+            <div className="top ">
+                <div className="title flex ">
+                    <h2>Total</h2>
+                    <FontAwesomeIcon icon={faEllipsisVertical} />
+                </div>
+            </div>
+            <div className="bottom flex column">
+                <div className="chart-container">
+                    <CircularProgressbar value={70} text="70%" strokeWidth={5}/>
+                </div>
+            </div>
+        </div>
     )
 }
 
