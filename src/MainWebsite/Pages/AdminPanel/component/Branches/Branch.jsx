7cb966273cec3cef7c913249d74cad54d3dca10e
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-01 00:41:55 +0300

    add table and import usestate

--- src/MainWebsite/Pages/AdminPanel/component/Branches/Branch.jsx
+++ src/MainWebsite/Pages/AdminPanel/component/Branches/Branch.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { useState,useEffect } from "react";
 
 const Branches = ()=>{
 
     return (
-        <div>Hello</div>
+        <div className="main-container flex column">
+            <div className="heading flex ">
+                <h1>Branch</h1>
+                <div>
+                    <button>Create</button>
+                </div>
+            </div>
+            <div className="table-container">
+            <table>
+                    <thead>
+                        <tr>
+                            <th>Branch ID</th>
+                            <th>Branch Name</th>
+                            <th>Address</th>
+                            <th>Manager Email</th>
+                            <th>Status</th>
+                            <th>Capacity</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    </table>
+            </div>
+         </div>
     )
 }
 export default Branches
