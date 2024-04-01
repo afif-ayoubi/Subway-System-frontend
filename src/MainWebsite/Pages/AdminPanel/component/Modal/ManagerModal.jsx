88b645b9ba8d6dedd2b63b54fefe18611f94a033
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-02 01:32:51 +0300

    add modal and update

---
+++ src/MainWebsite/Pages/AdminPanel/component/Modal/ManagerModal.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./ManagerModal.css"
+
+
+const ManagerModal = ({onclose})=>{
+return (
+    <div className="manager-cards flex center">
+        <div className="manager-card flex column">
+        <h2 className="flex center">Create New Station</h2>
+            <form className="flex column center">
+              <input type="text" name="name" placeholder="Name"  />
+              <input type="text" name="usename" placeholder="Username"  />
+              <input type="text" name="managerEmail" placeholder="Manager Email"  />
+              <input type="text" name="status" placeholder="Status" />
+              <div className="btns flex center">
+                 <button type="submit" className="add">Create</button>
+               <button onClick={onclose} className="del">Close</button>
+              </div>
+              
+            </form>
+        </div>
+       
+    </div>
+)
+    
+}
+
+export default ManagerModal
