353f1fbd4fe8a1e2fd14663db91110d9b2022898
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-02 02:29:18 +0300

    add rides

---
+++ src/MainWebsite/Pages/AdminPanel/ManagerPane;/Rides/Rides.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { useState,useEffect } from "react";
+import Modal from "../../component/Modal/modal";
+
+const Rides = ()=>{
+    const [rows, setRows] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [show,setShow] = useState(false)
+    const pageSize = 7;
+ 
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/todos/');
+                const data = await response.json();
+                setRows(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+    
+
+    const totalPages = Math.ceil(rows.length / pageSize);
+
+    const PageChange = (page) => {
+        setCurrentPage(page);
+    };
+    const Delete = (id) => {
+        setRows(prevRows => prevRows.filter(row => row.id !== id));
+    };
+
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    const displayRows = rows.slice(startIndex, endIndex);
+
+
+    return (
+        <div className="main-container flex column">
+    <div className="heading flex">
+        <h1>Rides</h1>
+        <div>
+            <button onClick={()=>{setShow(true)}}>Create</button>
+        </div>
+    </div>
+   { show && <Modal onclose = {() => setShow(false)}/>}
+
+   {!show && (
+                <>
+    <div className="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Arrival Time</th>
+                    <th>Dep Time</th>
+                    
+                    <th>Status</th>
+                    {/* <th></th>
+                    <th></th> */}
+                    <th colSpan="4">Action</th>
+                    {/* <th></th>
+                    <th></th> */}
+                </tr>
+            </thead>
+            <tbody>
+                {displayRows.map(row => (
+                    <tr key={row.id}>
+                        <td>{row.id}</td>
+                        <td>{row.name}</td>
+                        <td>{row.address}</td>
+                        <td>{row.managerEmail}</td>
+                        <td>{row.status}</td>
+                        <td>{row.capacity}</td>
+                        <td>
+                            <button onClick={() => Delete(row.id)}>Remove</button>
+                            
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+
+    <div className="pages flex">
+        <button onClick={() => PageChange(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
+        <span>{currentPage} of {totalPages}</span>
+        <button onClick={() => PageChange(currentPage + 1)} disabled={currentPage === totalPages}>Next</button>
+    </div>
+    </>
+            )}
+</div>
+
+    )
+}
+
+export default Rides
