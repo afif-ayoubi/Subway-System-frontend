67a09919211695ce8bf7e54348d372a1581418ba
MhaamadAli <m.alialaawar@gmail.com>
2024-04-03 23:19:57 +0300

    finished the header

--- src/MainWebsite/Pages/Landing/index.jsx
+++ src/MainWebsite/Pages/Landing/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Header from '../../Components/Header'
+import './style.css'
 
 const Landing = () => {
   return (
     <div>
-      <h1>Landing</h1>
+      <Header />
+
     </div>
   );
 };
