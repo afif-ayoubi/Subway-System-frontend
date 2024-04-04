90c3480256969cb8378bab100f81de91a422c9ff
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-05 02:21:49 +0300

    add ride modal

---
+++ src/MainWebsite/Pages/AdminPanel/component/Modal/RideModal.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './stationModal.css'
+
+
+const EditRidePopup = ({ onClose }) => {
+  return (
+    <div className="modal flex column center">
+      <div className="modal-content flex column">
+        <span className="close" onClick={onClose}></span>
+        <h2 className="flex center">Edit Station</h2>
+        <form className="flex column center">
+          <input type="text" name="name" placeholder="Name" />
+          <input type="text" name="address" placeholder="Address" />
+          <input type="text" name="Facilities" placeholder="Facilities" />
+          <input type="text" name="status" placeholder="Status" />
+          <input type="text" name="operation" placeholder="Operation Hours" />
+          <div className="btns flex center">
+            <button type="submit" className="add">Save</button>
+            <button onClick={onClose} className="del">Close</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default EditRidePopup;
