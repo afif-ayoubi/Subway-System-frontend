4236c877a7ae3d9a874937838746cc19b4460a32
MhaamadAli <m.alialaawar@gmail.com>
2024-04-04 22:06:49 +0300

    added popup for map pin

--- src/MainWebsite/Pages/Map/index.jsx
+++ src/MainWebsite/Pages/Map/index.jsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import {Icon} from 'leaflet'
 import "leaflet/dist/leaflet.css";
 import './style.css'
@@ -34,7 +34,11 @@ const index = () => {
           key={index}
           position={marker.geocode}
           icon={customIcon}
-        ></Marker>
+        >
+          <Popup>
+            {marker.popUp}
+          </Popup>
+        </Marker>
       ))}
     </MapContainer>
   )
