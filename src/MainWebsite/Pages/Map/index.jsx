4ffb78424291e5d4e3679d04543b7b86acf8e715
MhaamadAli <m.alialaawar@gmail.com>
2024-04-04 23:16:49 +0300

    fixed map design

--- src/MainWebsite/Pages/Map/index.jsx
+++ src/MainWebsite/Pages/Map/index.jsx
@@ -1,46 +1,51 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import {Icon} from 'leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { Icon } from "leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
+import Header from "../../Components/Header";
+import Footer from "../../Components/Footer";
+
 import "leaflet/dist/leaflet.css";
-import './style.css'
+import "./style.css";
+
 const index = () => {
   const markers = [
     {
       geocode: [48.86, 2.3522],
-      popUp: "Hello, I am pop up 1"
+      popUp: "Hello, I am pop up 1",
     },
     {
       geocode: [48.85, 2.3522],
-      popUp: "Hello, I am pop up 2"
+      popUp: "Hello, I am pop up 2",
     },
     {
       geocode: [48.855, 2.34],
-      popUp: "Hello, I am pop up 3"
-    }
+      popUp: "Hello, I am pop up 3",
+    },
   ];
 
   const customIcon = new Icon({
-    // iconUrl: "https://cdn-icons-png.flaticon.com/512/447/447031.png",
     iconUrl: require("./icons/placeholder.png"),
-    iconSize: [38, 38] 
+    iconSize: [38, 38],
   });
 
   return (
-    <MapContainer center={[48.8566, 2.3522]} zoom={13}>
-      <TileLayer 
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' 
-      />
-      {markers.map((marker, index) => (
-        <Marker
-          key={index}
-          position={marker.geocode}
-          icon={customIcon}
-        >
-          <Popup>
-            {marker.popUp}
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
-  )
-}
-export default index
+    <>
+      <Header />
+      <div className="map">
+        <MapContainer center={[48.8566, 2.3522]} zoom={13}>
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <MarkerClusterGroup chunkedLoading>
+            {markers.map((marker, index) => (
+              <Marker key={index} position={marker.geocode} icon={customIcon}>
+                <Popup>{marker.popUp}</Popup>
+              </Marker>
+            ))}
+          </MarkerClusterGroup>
+        </MapContainer>
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+export default index;
