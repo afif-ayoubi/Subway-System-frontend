377741103b5f46623858f691a634499689bb7bc7
afif-ayoubi <afifayoubi22@gmail.com>
2024-04-05 07:48:15 +0300

    review pop up

--- src/MainWebsite/Components/ReviewPopup/index.js
+++ src/MainWebsite/Components/ReviewPopup/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const ReviewPopup = () => {
+const ReviewPopup = ({ onClose }) => {
   const [rating, setRating] = useState(0);
 
   const handleStarClick = (clickedRating) => {
@@ -16,22 +16,36 @@ const ReviewPopup = () => {
   };
 
   return (
-    <div id="review-popup" className="review-popup flex center">
-      <div className="review-card off-white-bg ">
-        <div className="btn flex center">
-          <button className="flight-btn box-shadow primary-bg off-white-text border-radius" onClick={() => toggleVisibilityforpopUp(true)}>Flight</button>
-          <button className="airline-btn box-shadow onclick-nav-bg off-white-text border-radius" onClick={() => toggleVisibilityforpopUp(false)}>AirLine</button>
-        </div>
+    <div className="popup-container">
+      <div className="popup">
+        <h2>Leave a Review</h2>
         <div className="star-rate flex center">
           {[1, 2, 3, 4, 5].map((ratingValue) => (
-            <div key={ratingValue} className="stars" data-rating={ratingValue} onClick={() => handleStarClick(ratingValue)} style={{ color: highlightStars(ratingValue) }}>&#9733;</div>
+            <div
+              key={ratingValue}
+              className="stars"
+              data-rating={ratingValue}
+              onClick={() => handleStarClick(ratingValue)}
+              style={{ color: highlightStars(ratingValue) }}
+            >
+              &#9733;
+            </div>
           ))}
         </div>
-
-        <textarea name="" id="" cols="30" rows="5" placeholder="Describe Your Experience" className="light-bg"></textarea>
-        <div className="submit flex center ">
-          <button id="submit-btn" className="box-shadow primary-bg off-white-text border-radius">Submit</button>
+        <textarea
+          cols="30"
+          rows="5"
+          placeholder="Describe Your Experience"
+          className="light-bg"
+        ></textarea>
+        <div className="submit flex center">
+          <button className="box-shadow primary-bg off-white-text border-radius">
+            Submit
+          </button>
         </div>
+        <button className="close-btn" onClick={onClose}>
+          Close
+        </button>
       </div>
     </div>
   );
